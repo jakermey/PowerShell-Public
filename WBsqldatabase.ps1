3320bb642fbfacc1511dc528057fd0aeba14e7ae
Jake Meyers <24416361+jakermey@users.noreply.github.com>
2021-09-24 13:52:35 -0400

    Delete WBsqldatabase.ps1

--- WBsqldatabase.ps1
+++
@@ -1,5 +0,0 @@
-param()
-                    
-[String]$output = Get-Date -Date ((Get-Date).AddYears(1)) -UFormat %s
-$DeploymentScriptOutputs = @{}
-$DeploymentScriptOutputs['expireDate'] = $output
