28e49c034eba080f8cb6806c2caf347925562e0a
Jake Meyers <jakemeyers@microsoft.com>
2021-11-13 22:51:03 -0500

    Updated Outputs
    Added output for NBF ("Not Before Date" or "Activation Date")

--- ARM/WB/WBsqldatabase.ps1
+++ ARM/WB/WBsqldatabase.ps1
@@ -1,5 +1,7 @@
 param()
-                    
-[String]$output = Get-Date -Date ((Get-Date).AddYears(1)) -UFormat %s
+
+[string]$nowDate = Get-Date -UFormat
+[String]$yearDate = Get-Date -Date ((Get-Date).AddYears(1)) -UFormat %s
 $DeploymentScriptOutputs = @{}
-$DeploymentScriptOutputs['expireDate'] = $output
+$DeploymentScriptOutputs['nbf'] = $nowDate
+$DeploymentScriptOutputs['exp'] = $yearDate
